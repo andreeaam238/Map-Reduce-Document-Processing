380b795aeaaa9497dee440f9406125649478a280
Andreea Blaga <57097532+andreeaam238@users.noreply.github.com>
2022-02-21 11:17:27 +0200

    Add files via upload

---
+++ MapReduceResult.java
@@ -0,0 +1,36 @@
+// The result of a Reduce Task.
+public class MapReduceResult {
+    private double rank;
+    private int maxLength;
+    private int noMaxLengthWords;
+
+    public MapReduceResult(double rank, int maxLength, int noMaxLengthWords) {
+        this.rank = rank;
+        this.maxLength = maxLength;
+        this.noMaxLengthWords = noMaxLengthWords;
+    }
+
+    public double getRank() {
+        return rank;
+    }
+
+    public void setRank(double rank) {
+        this.rank = rank;
+    }
+
+    public int getMaxLength() {
+        return maxLength;
+    }
+
+    public void setMaxLength(int maxLength) {
+        this.maxLength = maxLength;
+    }
+
+    public int getNoMaxLengthWords() {
+        return noMaxLengthWords;
+    }
+
+    public void setNoMaxLengthWords(int noMaxLengthWords) {
+        this.noMaxLengthWords = noMaxLengthWords;
+    }
+}
